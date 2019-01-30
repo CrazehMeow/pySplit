d56b5d996ef7d7615dea79b45baa5aecfbc2d09a
cdbd25 <ChrisBodenbender@gmail.com>
2019-01-30 20:51:18 +0100

    implemented handling of command line arguments -a and -b

--- split.py
+++ split.py
@@ -21,6 +21,9 @@ import sys
 # default is 1000, modified by -l
 lines_per_file = 1000
 
+# no default is set
+bytes_per_file = 0
+
 # default is x
 output_file_name = "x"
 
@@ -33,6 +36,67 @@ suffix_length = 2
 
 print('Number of arguments:', len(sys.argv), 'arguments.')
 print('Argument List:', str(sys.argv))
+
+# 0 for default, 1 for lines, 2 for bytes, anything above = error
+mode_set = 0
+
+
+# filter out program name reference
+_, *args = sys.argv
+
+if "-l" in args and "-b" in args:
+    print("split: cannot split in more than one way")
+    exit(1)
+
+# process command line arguments
+x = 0
+while x < len(args):
+    if args[x] in ["-a", '-b', '-l']:
+        # test if there is an argument following the parameter
+        if len(args) <= x+1:
+            print("split: option requires an argument ", args[x])
+            exit(1)
+        parameterName = args[x]
+        parameterValue = args[x+1]
+
+        if parameterName == "-a":
+            # check if value is numeric
+            try:
+                suffix_length = int(parameterValue)
+            except ValueError:
+                exit(1)
+
+        if parameterName == "-b":
+            mode_set += 2
+            n, *size_token = parameterValue
+            is_size_token = False
+            print(n, size_token)
+            if len(size_token) > 0:
+                is_size_token = True
+                if len(size_token) == 1 and size_token[0] in ["k", "m"]:
+                    if size_token[0] == "k":
+                        size_token = 1024
+                    elif size_token[0] == "m":
+                        size_token = 1038576
+                else:
+                    print("split: invalid number of bytes:", parameterValue)
+                    exit(1)
+            try:
+                n = int(n)
+            except ValueError:
+                exit(1)
+            if is_size_token:
+                n = n * size_token
+            bytes_per_file = n
+        # increment by two for args -a, -b, -l
+        x += 2
+
+print("---DEBUG---")
+print("mode_set is ", mode_set)
+print("suffix_length is", suffix_length)
+print("lines_per_file is", lines_per_file)
+print("bytes_per_file is", bytes_per_file)
+
 
 # iterate over arguments, analyze each argument and react accordingly.
 
